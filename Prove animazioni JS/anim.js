f45f543c7c08aa36d14d5ca654efa517eddbcff1
Arianna Masciolini <uzkamascio@gmail.com>
2016-11-24 18:30:47 +0100

    aggiunto ridimensionamento dinamico cornice laterale

--- Prove animazioni JS/anim.js
+++ Prove animazioni JS/anim.js
@@ -1,3 +1,11 @@
+window.onload=function() {
+  frameResize()
+}
+
+window.onresize=function() {
+  frameResize()
+}
+
 function ltr() {
   var position=0
   function frame() {
@@ -7,4 +15,10 @@ function ltr() {
       clearInterval(id)
   }
   var id=setInterval(frame, 5)
+}
+
+function frameResize() {
+  var curtain_height=window.innerHeight
+  document.getElementById('sx').style.height=curtain_height+'px'
+  document.getElementById('dx').style.height=curtain_height+'px'
 }
