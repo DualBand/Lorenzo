237bd4dd157713af7f4a7198fbd4e7421765e884
Arianna Masciolini <uzkamascio@gmail.com>
2017-07-07 00:50:37 +0200

    Migliorata pagina progetti (aggiunta effetti al passaggio del mouse).

--- sito/js/anim.js
+++ sito/js/anim.js
@@ -1,7 +1,7 @@
 function ltr() {
   var position=0
   function frame() {
-    position+=2.5
+    position+=3
     document.getElementById('sipario').style.left=position+'px'
     if (position==2000)
       clearInterval(id)
