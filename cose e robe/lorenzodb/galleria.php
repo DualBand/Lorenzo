f5937e7485ef0cffbb648b288a9617ec19d8b3c2
Arianna Masciolini <uzkamascio@gmail.com>
2017-10-09 17:30:09 +0200

    Differenziazione output a seconda del tipo di file (video/immagine) e cancellazione di alcuni commenti supreflui.

--- cose e robe/lorenzodb/galleria.php
+++ cose e robe/lorenzodb/galleria.php
@@ -5,23 +5,26 @@
       <link rel='shortcut icon' type='image/x-icon' href='../img/icon.ico' />
       <meta charset="utf-8">
       <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
-      <link rel='stylesheet' type='text/css' media='screen and (min-device-width: 800px)' href='../sito/css/style.css'>
+      <!--<link rel='stylesheet' type='text/css' media='screen and (min-device-width: 800px)' href='../sito/css/style.css'>-->
       <link rel='stylesheet' type='text/css' media='screen and (max-device-width: 799px)' href='../css/mstyle.css'>
       <script type='text/javascript' src='../js/re.js'></script>
    </head>
 
    <body>
       <table>
-            <?php
-               include "connessione.php";
-               $par=$_GET['project'];
-               $sql="SELECT titolo, descrizione, media FROM ".$tabella." WHERE progetto=\"".$par."\"";
-               $result = mysqli_query($conn, $sql); //lancio query
-
-               if (mysqli_num_rows($result) > 0) { //costruzione tabella (da modificare per visualizzazione video)
-             while($row = mysqli_fetch_assoc($result)) {
-                 echo "<tr><td>" . $row["titolo"]. "</td></tr><tr><td>" . $row["descrizione"]. "</td></tr><tr><td><img alt=\"media\" src=\"media/" . $row["media"]."\"></td></tr><tr><td><hr></td></tr><tr><td><hr></td></tr><br>";
-             }
+         <?php
+            include "connessione.php";
+            $par=$_GET['project'];
+            $sql="SELECT titolo, descrizione, media FROM ".$tabella." WHERE progetto=\"".$par."\"";
+            $result = mysqli_query($conn, $sql); //lancio query
+            if (mysqli_num_rows($result) > 0) { //costruzione tabella (da modificare per visualizzazione video)
+            while($row = mysqli_fetch_assoc($result)) {
+               $fileType = pathinfo($row["media"],PATHINFO_EXTENSION);
+               if($fileType == "mp4" || $fileType == "avi" || $fileType == "webm") {
+                  echo "<video controls><source src=\"media/" . $row["media"]."\" type=\"video/" . $fileType ."\">Your browser does not support the video tag.</video>";
+               }
+               else echo "<img alt=\"media\" src=\"media/" . $row["media"]."\">";
+            }
          } else {
             //è stato bello...
          }
--- cose e robe/lorenzodb/upload.php
+++ cose e robe/lorenzodb/upload.php
@@ -9,67 +9,54 @@
 <body>
 <br>
 
-<?php $target_dir = "media/"; /// cartella che contiene le immagini
+<?php $target_dir = "media/";
 define('MB', 1048576);
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]); /// indirizzo completo su dove copiare
-$uploadOk = 1; /// variabile di controllo mi serve per capire se tutto e' andato ok
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
 $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
 $nuovo_id=0;
 $nome_file_scelto="";
 $nuovo_progetto="";
 $id=0;
 
-
-
-// Check se il file e' gia' esistente
 if (file_exists($target_file)) {
     echo "Spiacente, c'è già un file con questo nome nel database.";
-    $uploadOk = 0; /// non va bene
+    $uploadOk = 0;
 }
 if ($_FILES["fileToUpload"]["size"] > 50*MB) {
     echo "Spiacente, il tuo file e' troppo grosso.";
-    $uploadOk = 0; /// non va bene
+    $uploadOk = 0;
 }
 
 if($fileType != "jpg" && $fileType != "JPG" && $fileType != "png" && $fileType != "jpeg" && $fileType != "gif" && $fileType != "mp4" && $fileType != "avi" && $fileType != "webm") { //controllo formato file
     echo "Formato non supportato. Se il tuo file è un immagine o un video, contatta i creatori del sito.";
-    $uploadOk = 0;  /// non va bene
+    $uploadOk = 0;
 }
 
-
-// Check se $uploadOk è a 0, cioè qualcosa non funziona oppure è a 1 cioè tutto funziona
 if ($uploadOk == 0) {
-    echo "Spiacente, qualcosa non ha funzionato.";
-// se tutto è a posto provo ad uplodare il file selezionato
-} else { // solo se tutto è a posto visualizzo il resto altrimenti si ferma qui'
+    echo "Spiacente, qualcosa è andato storto.";
+} else {
 
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {  /// invio del file
-      echo "IL file ". basename( $_FILES["fileToUpload"]["name"]). "<br> e' stato caricato.<br>"; // il nome del file a monitor è stato caricato
-
-$nome_file_scelto=basename( $_FILES["fileToUpload"]["name"]); //immagazzino il nome del file completo in una variabile di nome $nome_file_scelto
-$nuovo_titolo=$_SESSION["variabile1"]; //immagazzino il titolo in una variabile di nome $nuovo_titolo, il dato lo avevo messo dentro la sessione
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "IL file ". basename( $_FILES["fileToUpload"]["name"]). "<br> e' stato caricato.<br>";
+$nome_file_scelto=basename( $_FILES["fileToUpload"]["name"]); variabile di nome $nome_file_scelto
+$nuovo_titolo=$_SESSION["variabile1"];
 $nuova_descrizione=$_SESSION["variabile2"];
 $nuovo_progetto=$_SESSION["variabile3"];
 
 
-include"connessione.php"; //al fine di connettermi al database mysql,ho inserito in una pagina esterna dal nome "connessione.php"  i dati nome utente e passw, oltre alla connessione del db mysql
+include"connessione.php";
 
+$sql = "SELECT id FROM lavori";
+$result = mysqli_query($conn, $sql);
 
-$sql = "SELECT id FROM lavori"; // seleziono dalla tabella del database il numero progressivo dei lavori inseriti.
-$result = mysqli_query($conn, $sql); // lancio della query che mi entra dentro $result
-
-    // leggo ogni riga del database, lo scopo è di avere l'ultimo numero inserito per inserire il numero successivo.
     while($row = mysqli_fetch_assoc($result)) {
-        $id=$row["id"]; // immagazzino il numero progressivo ultimo in $id
+        $id=$row["id"];
     }
-$nuovo_id=$id+1; // il prossimo lavoro che inseriro' avra' l'id +1
+$nuovo_id=$id+1; // il prossimo lavoro avra' id=id+1
 
-
-//sotto chiedo la conferma se vuoi procedere a inserire i dati nel database
 echo"<br><br>devo inserire in db?<br><br> id=$nuovo_id --- <br>titolo=$nuovo_titolo ---<br> descrizione=$nuova_descrizione ---<br> media=$nome_file_scelto<br><br>????";
-// di sotto c'è una cosa un po' anomala, ossia con il php costruisco il form per inviare i dati al database.
-// la differenza rispetto a scrivere il form con l'html, cosi' come nella pagina inserisci_lavoro.html è che dove ci sono gli apici " devo mettergli davanti al barra rovesciata \
-// uso questa tecnica (costruire l'html con il php) perchè solo se tutto è ok te lo fa vedere.
+
 echo"
 <form action=\"inserisci_db.php\" method=\"post\">
 titolo<br>
@@ -86,18 +73,11 @@ titolo<br>
 </form>
 ";
 
-
-
     } else {
-        echo "Spiacente c'è un errore a caricare il file.";
+        echo "Spiacente, si è verificato un errore nel caricamento del file.";
     }
-} // qui' finisce la parte che si visualizza e si attiva se il file era stato caricato.
-
-
-
+}
 
 ?>
-<br>
-
 <br>
 </body></html>
